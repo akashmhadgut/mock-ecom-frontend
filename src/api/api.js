72542aece379c21bf4cefb4f5c1f0b04aa1c9fb2
akashmhadgut <akashmhadgut280@gmail.com>
2025-11-07 11:14:43 +0530

    Update API baseURL to deployed backend

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://mock-ecom-backend.onrender.com/api",
+  baseURL: "https://mock-ecom-backend-1.onrender.com/api",
 });
 
 
